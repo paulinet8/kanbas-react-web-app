8e8e22d485929ce99b20dc2475df3128109ae149
paulinet8 <97944875+paulinet8@users.noreply.github.com>
2024-07-08 00:53:25 -0700

    Update index.tsx
    Added full name and class section

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -10,6 +10,7 @@ import Lab3 from "./Lab3";
 export default function Labs() {
   return (
     <div id="wd-labs">
+      <h1>Pauline Truong CS5610 Summer2 2024</h1>
       <h1>Labs</h1>
       <TOC />
       <Routes>
